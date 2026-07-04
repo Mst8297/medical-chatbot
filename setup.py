d2880a3ae2a8771245d9b81e8e61b978f8e497fb
mst8297 <esterm8297@gmail.com>
2026-07-04 19:55:51 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptool import find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Ester",
+    author_email="estermoiseyev@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
